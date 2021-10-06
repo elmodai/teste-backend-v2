49f02c5c9bd197a928b0a4b04cfa1cf9fbe857b2
elmodai <elmodai@gmail.com>
2021-10-06 11:01:45 +0200

    Projects added

---
+++ GestEquipamento/src/Aiko.GestEquipamento.Infra.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Aiko.GestEquipamento.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ GestEquipamento/src/Aiko.GestEquipamento.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Aiko.GestEquipamento.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
