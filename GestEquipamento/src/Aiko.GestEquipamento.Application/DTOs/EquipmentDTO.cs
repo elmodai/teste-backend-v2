a3cce955402a1976ae55ddabfb739d11dd1afee3
elmodai <elmodai@gmail.com>
2021-10-10 19:22:17 +0200

    Equipment Post method Done

---
+++ GestEquipamento/src/Aiko.GestEquipamento.Application/DTOs/EquipmentDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Aiko.GestEquipamento.Application.DTOs
+{
+    public class EquipmentDTO
+    {
+        public EquipmentDTO()
+        {
+        }
+    }
+}
---
+++ GestEquipamento/src/Aiko.GestEquipamento.PresentationAPI/Controllers/EquipmentController.cs
@@ -0,0 +1,7 @@
+namespace Aiko.GestEquipamento.PresentationAPI.Controllers
+{
+    public class EquipmentController : ControllerBase
+    {
+        
+    }
+}
