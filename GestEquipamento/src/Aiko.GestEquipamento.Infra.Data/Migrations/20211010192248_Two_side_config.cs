272bdf9c4e29f7d35890e61929b334eac2e66ee9
elmodai <elmodai@gmail.com>
2021-10-10 21:48:13 +0200

    Final project structures

---
+++ GestEquipamento/src/Aiko.GestEquipamento.Infra.Data/Migrations/20211010192248_Two_side_config.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aiko.GestEquipamento.Infra.Data.Migrations
+{
+    public partial class Two_side_config : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ GestEquipamento/src/Aiko.GestEquipamento.PresentationAPI/Controllers/EquipmentModelStateHourlyEarningsController.cs
@@ -0,0 +1,12 @@
+namespace Aiko.GestEquipamento.PresentationAPI.Controllers
+{
+    [ApiController]
+    [Route("equipmentsearnings")]    
+    public class EquipmentModelStateHourlyEarningsController
+    {
+        public EquipmentModelStateHourlyEarningsController(Parameters)
+        {
+            
+        }
+    }
+}
