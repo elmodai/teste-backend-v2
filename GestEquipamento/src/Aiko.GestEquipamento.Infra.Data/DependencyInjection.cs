2765d236abe024b2c79b019f7993ab63103d7613
elmodai <elmodai@gmail.com>
2021-10-10 13:02:32 +0200

    Setting projects references

---
+++ GestEquipamento/src/Aiko.GestEquipamento.Infra.Data/DependencyInjection.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aiko.GestEquipamento.Infra.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            return null;
+        }
+    }
+}
